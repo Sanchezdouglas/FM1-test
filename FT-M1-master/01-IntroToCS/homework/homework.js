f02a173ae248cd0d117754a7249257a8df61a068
sancehzdouglas <97553140+Sanchezdouglas@users.noreply.github.com>
2022-08-03 21:18:51 +0200

    Act-Ej-01

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let arr= num.toString().split("").reverse().join("")
+  let Decimal = 0;
+  for(let i=0; i<arr.length;i++) {
+    Decimal += (Math.pow(2,1)*arr[i])
+  }
+  return Decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let arr = [];
+  let resto = 0;
+  while(num>0) {
+    resto = num % 2;
+    num = Math.floor(num/2);
+    arr.unshift(resto);
+  }
+  return arr.joi("");
 }
 
 
